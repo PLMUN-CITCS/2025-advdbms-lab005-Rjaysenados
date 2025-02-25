60cc2fb2287a8e32153eac73c57b8938b6c00a12
rjaysenados <senadosr-jay_bsit@plmun.edu.ph>
2025-02-26 01:15:13 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 03

--- university_db/insert_students.sql
+++ university_db/insert_students.sql
@@ -1,5 +1,9 @@
-INSERT INTO  `Students` (`FirstName`, `LastName`, `EnrollmentDate`, `Email`)
-VALUES
-('Alice', 'Smith', '2023-09-01', 'alice.smith@example.com'),
-('Bob', 'Johnson', '2023-09-01', 'bob.johnson@example.com'),
-('Charlie', 'Lee', '2023-09-01', 'charlie.lee@example.com');
+CREATE TABLE `Students` (
+   `StudentID` INT AUTO_INCREMENT PRIMARY KEY,
+   `FirstName` VARCHAR(50) NOT NULL,
+   `LastName` VARCHAR(50) NOT NULL,
+   `EnrollmentDate` DATE
+);
+
+ALTER TABLE `Students`
+ADD `Email` VARCHAR(100);
